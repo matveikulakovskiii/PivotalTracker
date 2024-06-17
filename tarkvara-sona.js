05e44da23f1a9a205b677c9233310bd783e04477
matveikulakovskiii <120181570+matveikulakovskiii@users.noreply.github.com>
2024-06-17 11:29:44 +0300

    [#187805018] - Update tarkvara-sona.js

--- tarkvara-sona.js
+++ tarkvara-sona.js
@@ -70,3 +70,73 @@ function kontrollimine(){
 }
 
 
+function randomSona2(){
+
+    //massiivi loomine vene sõnad
+    const sonad3=[
+        'реализация',
+        'алгоритм',
+        'проект',
+        'пользовательский инткрфейс',
+        'модель',
+        'схема базы данных',
+        'стандарт',
+        'цикл',
+        'обработка данных',
+        'структура данных',
+        'среда разработки',
+        'команда управления проектом',
+        'планирование',
+        'итерация',
+        'коррекция',
+        'корректирование'
+    ];
+
+    //random sõna
+    const juhuslikSona2=Math.floor(Math.random()*sonad3.length);
+
+    //võtame random sõna massivist
+    const sona=sonad3[juhuslikSona2];
+
+    //lisame html-lehele
+    document.getElementById("random-sona2").innerHTML=sona;
+
+    //juhuslikult valitud eestikeelse sõna indeksi salvestamine
+    currentIndex = juhuslikSona2;
+}
+
+function kontrollimine2(){
+    const a="Õige";
+    const b="Vale";
+
+    //massiivi loomine eest sõnad
+    const sonad4=[
+        'teostus',
+        'algoritm',
+        'kavand',
+        'kasutajaliides',
+        'mudel',
+        'andmebaasi skeem',
+        'standart',
+        'tsükkel',
+        'andmetöötus',
+        'andmestruktur',
+        'arendusskeskkond',
+        'projektihaldus töörist',
+        'kavandaminem',
+        'iteratsioon',
+        'parandus',
+        'parandamine'
+    ];
+
+
+    //kasutaja kirjutatud sõna
+    const sona=document.getElementById("kontroll2").value;
+
+    //valideerimine
+    if (currentIndex >= 0 && sona === sonad4[currentIndex]) {
+        document.getElementById("vastus2").innerHTML = a;
+    } else {
+        document.getElementById("vastus2").innerHTML = b;
+    }
+}
